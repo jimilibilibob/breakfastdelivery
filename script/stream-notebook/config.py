1113ede6c96ed77e8301f2863fdd975464934ab1
Thomas <thomas.fontaine@cleyrop.com>
2022-10-03 23:25:01 +0200

    fix: refacto notebook and add example

---
+++ script/stream-notebook/config.py
@@ -0,0 +1,6 @@
+config = {
+    "median_time": 5,
+    "kafka_uri"  : "51.210.210.181:9094",
+    "mongo_uri"  : "mongodb+srv://user_read:supertp2022@cluster0.vk9rt.mongodb.net/test",
+    "topic"      : "thorders"
+}
